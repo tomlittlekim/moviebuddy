5254e5c71f2bff31ff779f0476501587936cb67a
Youngmin Kim <youngmini90@naver.com>
2022-07-18 22:09:26 +0900

    section_10: Refactor metadata read strategy by profiles

--- src/test/java/moviebuddy/data/JaxbMovieReaderTest.java
+++ src/test/java/moviebuddy/data/JaxbMovieReaderTest.java
@@ -1,14 +1,17 @@
 package moviebuddy.data;
 
 import moviebuddy.MovieBuddyFactory;
+import moviebuddy.MovieBuddyProfile;
 import moviebuddy.domain.Movie;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.util.List;
 
+@ActiveProfiles(MovieBuddyProfile.XML_MODE)
 @SpringJUnitConfig(MovieBuddyFactory.class)
 public class JaxbMovieReaderTest {
 
